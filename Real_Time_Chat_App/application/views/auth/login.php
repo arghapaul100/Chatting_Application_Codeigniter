5e412d5047d7ad846f50f56e02fd93057649bed0
Argha Paul <arghapaul100@gmail.com>
2021-05-26 22:51:53 -0700

    Chatting Application in Codeigniter
    This is my first chatting application using codeigniter, jquery, javascript, ajax and bootstrap4.

---
+++ Real_Time_Chat_App/application/views/auth/login.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.3/css/all.css" integrity="sha384-SZXxX4whJ79/gErwcOYf+zWLeJdY/qpuqC4cAa9rOGUstPomtqpuNWT9wdPEn2fk" crossorigin="anonymous">
+
+	<link rel="stylesheet" type="text/css" href="./assets/css/login/style.css">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+</head>
+<body>
+	<section id="main">
+		<div id="" class="container form_container">
+			<div class="row form_row">
+				<div class="col-lg-4 bg-white px-3 py-3" id="form_col">
+					<form autocomplete="off" id="form_login">
+						<h3 class="text-center">Login account</h3>
+						<h6 class="text-center">Create your account ? <a href="index.php/signup">Link</a></h6>
+						<div class="alert alert-danger d-none" id="alert"></div>
+						<div id="ruler" class="">
+							<hr/>
+						</div>
+						<div class="field_container">
+							<div class="form-group">
+									<input type="email" name="txt_email" id="email_addr" placeholder="Email" class="form-control">
+							</div>
+							<div class="form-group mb-0">
+								<input type="password" name="txt_pass" placeholder="Password" class="form-control" id="pass1">
+							</div>
+							<div class="form-group my-0 pt-2 justify-content-end d-flex">
+								<div>
+									<input type="checkbox" name="toggle_pass" id="chkbox1">
+									<label for="chkbox1" class="mr-0">Show Password</label>
+								</div>
+							</div>
+							<div class="form-group">
+								<button class="btn btn-block" id="btn_login">
+									<span>Log in</span>
+								</button>
+							</div>
+
+						</div>
+						
+					</form>
+				</div>
+			</div>
+		</div>
+		
+	</section>
+
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.6.1/gsap.min.js"></script>
+	<script src="./assets/js/login/main.js"></script>
+</body>
+</html>
---
+++ Real_Time_Chat_App/database/chatapp.sql
@@ -0,0 +1,85 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: May 27, 2021 at 04:04 AM
+-- Server version: 10.4.17-MariaDB
+-- PHP Version: 8.0.2
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `chatapp`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `block_user`
+--
+
+CREATE TABLE `block_user` (
+  `blocked_from` varchar(10) COLLATE utf8mb4_bin NOT NULL,
+  `blocked_to` varchar(10) COLLATE utf8mb4_bin NOT NULL,
+  `time` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `id` int(20) NOT NULL,
+  `unique_id` varchar(20) CHARACTER SET latin1 NOT NULL,
+  `user_fname` varchar(50) CHARACTER SET latin1 NOT NULL,
+  `user_lname` varchar(30) CHARACTER SET latin1 NOT NULL,
+  `user_email` varchar(50) CHARACTER SET latin1 NOT NULL,
+  `bio` varchar(100) CHARACTER SET latin1 NOT NULL,
+  `created_at` varchar(20) COLLATE utf8mb4_bin NOT NULL,
+  `dob` varchar(20) COLLATE utf8mb4_bin NOT NULL,
+  `phone` varchar(20) COLLATE utf8mb4_bin NOT NULL,
+  `address` varchar(200) COLLATE utf8mb4_bin NOT NULL,
+  `user_pass` varchar(20) CHARACTER SET latin1 NOT NULL,
+  `user_avtar` varchar(200) CHARACTER SET latin1 NOT NULL,
+  `user_status` varchar(10) CHARACTER SET latin1 NOT NULL,
+  `last_logout` varchar(30) CHARACTER SET latin1 NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_messages`
+--
+
+CREATE TABLE `user_messages` (
+  `time` datetime(6) NOT NULL,
+  `sender_message_id` varchar(20) CHARACTER SET latin1 NOT NULL,
+  `receiver_message_id` varchar(20) CHARACTER SET latin1 NOT NULL,
+  `message` varchar(500) CHARACTER SET latin1 NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`unique_id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
